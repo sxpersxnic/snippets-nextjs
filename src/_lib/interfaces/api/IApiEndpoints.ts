18c8d785e1bb073b33c23a930861583369314ad9
sxpersxnic <102521739+sxpersxnic@users.noreply.github.com>
2024-12-08 23:04:19 +0100

    Update IApiEndpoints.ts

--- src/_lib/interfaces/api/IApiEndpoints.ts
+++ src/_lib/interfaces/api/IApiEndpoints.ts
@@ -1,13 +1,16 @@
 /* eslint-disable semi */
 /* eslint-disable no-unused-vars */
 import HttpMethod from 'lib/enums/EHttpMethods';
+import URI from 'lib/enums/EURL';
+import Request from './payload/request/IRequestDTO';
+import Response from './payload/response/IResponseDTO';
 
 export default interface IApiEndpoint {
-	url: string;
+	url: URI | string;
 	method: HttpMethod;
-	handler: () => Promise<>;
+	handler: (req: Request, res: Response) => Promise<Response>;
 	payload: {
-	  request?: ;
-	  response?: ;
+	  request?: Request;
+	  response?: Response;
 	};
 }
