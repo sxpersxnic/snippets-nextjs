6e080ab7f7e15d898617f4852d565b5a67fd2d85
sxpersxnic <102521739+sxpersxnic@users.noreply.github.com>
2024-12-08 17:59:28 +0100

    Update EHttpMethods.ts

--- src/_lib/enums/EHttpMethods.ts
+++ src/_lib/enums/EHttpMethods.ts
@@ -3,7 +3,12 @@ enum EHttpMethod {
 	GET = 'get',
 	POST = 'post',
 	PUT = 'put',
+	PATCH = 'patch',
 	DELETE = 'delete',
+	OPTIONS = 'options',
+	HEAD = 'head',
+	CONNECT = 'connect',
+	TRACE = 'trace'
 }
 
 export default EHttpMethod;
