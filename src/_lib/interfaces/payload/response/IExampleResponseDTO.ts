32b833d8adc905f9594692e8137e0cf8d064d1b5
sxpersxnic <102521739+sxpersxnic@users.noreply.github.com>
2024-12-08 18:21:34 +0100

    Update IExampleResponseDTO.ts

--- src/_lib/interfaces/payload/response/IExampleResponseDTO.ts
+++ src/_lib/interfaces/payload/response/IExampleResponseDTO.ts
@@ -1,7 +1,11 @@
 import Role from '../../../enums/ERole';
+import IResponseDTO from './IResponseDTO';
 
-export default interface IExampleResponseDTO {
-  id: string;
-  token: string;
-  role: Role;
+export default interface IExampleResponseDTO extends IResponseDTO {
+  header: {};
+  body: {
+    id: string;
+    token: string;
+    role: Role;
+  };
 }
