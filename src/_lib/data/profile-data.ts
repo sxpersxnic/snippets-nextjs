a053d9e7b82bbf5fff2b817c29017157d9ebfffc
sxpersxnic <melvin@kampus.ch>
2024-12-13 22:53:00 +0100

    Profile view

--- src/_lib/data/profile-data.ts
+++ src/_lib/data/profile-data.ts
@@ -1,21 +1,33 @@
 'use server';
 
-import { SelectProfile } from 'lib/types/model';
-import { profiles } from 'db/schema';
-import { eq } from 'drizzle-orm';
+import { notFound } from 'next/navigation';
 import { createClient } from '../utils/supabase/server';
+import Profile from 'lib/types/model/profile';
 
 const db = await createClient();
 
-export async function getProfileByUserId(id: string) {
+export async function getProfileByUserId(id: string): Promise<Profile> {
 	try {
-		const { data, error, status } = await db.from('profiles').select().eq('user_id', id).single();
+		const { data, error, status } = await db
+			.from('profiles')
+			.select('id, user_id, username, description, photo')
+			.eq('user_id', id)
+			.single();
 
 		if (error && status !== 406) {
 			throw error;
 		}
 
-		return data;
+		const profile: Profile = {
+			id: data?.id,
+			user_id: data?.user_id,
+			username: data?.username,
+			description: data?.description,
+			photo: data?.photo,
+		};
+		
+		return profile;
+
 	} catch (error) {
 		console.error('Failed to fetch profile: ', error);
 		throw new Error('Failed to fetch profile.');
--- src/_lib/types/model.ts
+++
@@ -1,5 +0,0 @@
-import { InferInsertModel, InferSelectModel } from 'drizzle-orm';
-import { profiles } from 'db/schema';
-
-export type NewProfile = InferInsertModel<typeof profiles>;
-export type SelectProfile = InferSelectModel<typeof profiles>;
