a8389978d614c650d0ce2671411e7e88657a674d
sxpersxnic <melvin@kampus.ch>
2024-12-13 22:35:42 +0100

    Profile view

--- src/_lib/data/profile-data.ts
+++ src/_lib/data/profile-data.ts
@@ -3,17 +3,19 @@
 import { SelectProfile } from 'lib/types/model';
 import { profiles } from 'db/schema';
 import { eq } from 'drizzle-orm';
-import { db } from '@/_db';
+import { createClient } from '../utils/supabase/server';
 
-export async function getProfileByUserId(id: string): Promise<SelectProfile> {
+const db = await createClient();
+
+export async function getProfileByUserId(id: string) {
 	try {
-		const data: SelectProfile[] = await db
-			.select()
-			.from(profiles)
-			.where(eq(profiles.userId, id));
-		const profile: SelectProfile = data[0];
+		const { data, error, status } = await db.from('profiles').select().eq('user_id', id).single();
 
-		return profile;
+		if (error && status !== 406) {
+			throw error;
+		}
+
+		return data;
 	} catch (error) {
 		console.error('Failed to fetch profile: ', error);
 		throw new Error('Failed to fetch profile.');
