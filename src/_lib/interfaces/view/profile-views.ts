e67c22c6817784678e49354600cc5df46e79cd83
sxpersxnic <melvin@kampus.ch>
2024-12-13 06:41:48 +0100

    Init

---
+++ src/_lib/interfaces/view/profile-views.ts
@@ -0,0 +1,11 @@
+export default interface ProfileView {
+  username: string;
+  photo: string;
+  description: string;
+}
+
+export default interface ProfilePreview {
+  id: string;
+  username: string;
+  photo: string;
+}
---
+++ src/_lib/interfaces/view/snippet-views.ts
@@ -0,0 +1,19 @@
+export interface SnippetView {
+  id: string;
+  profileId: string;
+  username: string;
+  photo: string;
+  title: string;
+  content: string;
+  language: string;
+  at: Date;
+}
+
+export interface SnippetView {
+  id: string;
+  profileId: string;
+  username: string;
+  photo: string;
+  title: string;
+  at: Date;
+}
